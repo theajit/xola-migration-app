a8d8cffe584d1e89c4fbee768342519f7b30c073
Ajit Kumar Satpathy <ajit@theajit.in>
2016-04-25 20:18:50 +0530

    clean up api call

--- xola-user-api.php
+++ xola-user-api.php
@@ -1,12 +1,9 @@
 <?php
-
 function admin_api_fetch()
 {
-
     $d_exp_url = $_POST['d_exp_url'];
     $d_username = $_POST['d_user_name'];
     $d_admin_password = $_POST['d_admin_password'];
-
     $ch_d_id = curl_init();
     curl_setopt($ch_d_id, CURLOPT_URL, $d_exp_url . '/api/users/me');
     curl_setopt($ch_d_id, CURLOPT_RETURNTRANSFER, 1);
@@ -15,14 +12,10 @@ function admin_api_fetch()
     curl_setopt($ch_d_id, CURLOPT_USERPWD, $d_username . ':' . $d_admin_password);
     curl_setopt($ch_d_id, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch_d_id, CURLOPT_SSL_VERIFYPEER, 0);
-
     $data_d = curl_exec($ch_d_id);
-
     $response_d = json_decode($data_d, TRUE);
-
     $err_d = curl_error($ch_d_id);
     curl_close($ch_d_id);
-
     if ($err_d) {
         echo "cURL Error #:" . $err_d;
     } else {
@@ -35,33 +28,25 @@ function admin_api_fetch()
         curl_setopt($ch, CURLOPT_USERPWD, $d_username . ':' . $d_admin_password);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
         $data_api = curl_exec($ch);
         $response_api = json_decode($data_api, TRUE);
         curl_close($ch);
         return $response_api[0];
     }
 }
-
 $i = $_POST['d_exp_url'];
 $j = $_POST['seller_username'];
-
 $apiKey_user = admin_api_fetch();
-
 $ch_id = curl_init();
 curl_setopt($ch_id, CURLOPT_URL, $i . '/api/users?private=true&type=1&limit=100&search=' . $j);
 curl_setopt($ch_id, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch_id, CURLOPT_CUSTOMREQUEST, "GET");
 curl_setopt($ch_id, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 curl_setopt($ch_id, CURLOPT_HTTPHEADER, array("x-api-key:" . $apiKey_user));
-
 $data_users = curl_exec($ch_id);
-
 $response = json_decode($data_users, TRUE);
-
 $err = curl_error($ch_id);
 curl_close($ch_id);
-
 if ($err) {
     echo "cURL Error #:" . $err;
 } else {
@@ -72,10 +57,10 @@ if ($err) {
     curl_setopt($ch_enable, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch_enable, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
     curl_setopt($ch_enable, CURLOPT_HTTPHEADER, array("x-api-key:" . $apiKey_user));
-
     $data = curl_exec($ch_enable);
-
     $err = curl_error($ch_enable);
     curl_close($ch_enable);
+    $enabled = TRUE;
+    return $enabled;
 }
 ?>
